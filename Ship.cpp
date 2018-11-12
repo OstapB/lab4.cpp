8e2cf138c98ba77b4d847eaed2e89e5807e01294
Ostap Baranovskiy <ostap.baranovski@gmail.com>
2018-11-12 20:43:40 +0200

    add cpp file from lab 4

---
+++ Ship.cpp
@@ -0,0 +1,13 @@
+#include "ship.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+	Ship ship;
+	ship.Ship2(2000, "Bird", 40);
+	ship.Ship3(3000, "Laguna", 35);
+
+	system("pause");
+	return 0;
+}
